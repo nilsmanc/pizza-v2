de3c303dfe8e492f84b02345a39081f959b21122
nilsmanc <etereaiberia@gmail.com>
2022-06-13 16:37:00 +0300

    router

---
+++ src/components/NotFoundBlock/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './NotFoundBlock.module.scss';
+
+export const NotFoundBlock = () => {
+  return (
+    <div className={styles.root}>
+      <h1>
+        <span>:(</span>
+        <br />
+        Ничего не найдено
+      </h1>
+      <p className={styles.description}>
+        К сожалению данная страница отсутствует в нашем интернет-магазине
+      </p>
+    </div>
+  );
+};
+
+export default NotFoundBlock;
